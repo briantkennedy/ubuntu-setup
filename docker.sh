bc54d004c85bad4d59b0e791f5054a192acf99e6
Brian Kennedy <btkennedy@gmail.com>
2015-10-20 13:44:15 -0700

    Add docker setup

--- docker.sh
+++ docker.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-source $(dirname $0)/common.sh
+dir="$(dirname $0)"
+source $dir/common.sh
 
+# from https://docs.docker.com/installation/ubuntulinux/
+list=/etc/apt/sources.list.d/docker.list
+if [ ! -e $list ]; then
+sudo apt-key adv \
+  --keyserver hkp://pgp.mit.edu:80 \
+  --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+cp -f $dir/etc/apt/sources.list.d/docker.list  $list
+sudo apt-get update
+fi
 
+agi docker-engine
 
+# set up docker group
+sudo usermod -aG docker $USER
